c0f73a3cc29366993e0a195ebfe0eef7a0018e0c
NagaViki <jnv280895@gmail.com>
2021-10-06 20:16:00 +0530

    finalupdatedcode

--- src/org/greens/test/login/Swiggy.java
+++ src/org/greens/test/login/Swiggy.java
@@ -20,25 +20,9 @@ public class Swiggy {
 		WebElement locat = driver.findElement(By.id("location"));
 
 		locat.sendKeys("chennai");
-	
-	
-	     System.setProperty("webdriver.chrome.driver","C:\\Users\\NAGA\\OneDrive\\Desktop\\MY CODE\\Selenium-day-2\\driver\\chromedriver.exe");
-			
-			WebDriver driver = new ChromeDriver();
-			
-			driver.manage().window().maximize();
-			
-			driver.get("https://www.swiggy.com");
-			
-			
-		 WebElement locat = driver.findElement(By.id("location"));
-		 
-				
-			locat.sendKeys("chennai");
+
 			
 			System.out.println(locat.getAttribute("value"));
 			
-			
-
 	}
 }
