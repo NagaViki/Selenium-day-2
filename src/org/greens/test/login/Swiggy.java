70f5b0fc738cab4c0764a670068ab10a4fcd03c7
NagaViki <jnv280895@gmail.com>
2021-10-06 20:07:50 +0530

    newup

--- src/org/greens/test/login/Swiggy.java
+++ src/org/greens/test/login/Swiggy.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Swiggy {
-
-
 	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver",
@@ -15,15 +13,32 @@ public class Swiggy {
 
 		WebDriver driver = new ChromeDriver();
 
-		driver.manage().window().maximize();
+		Thread.sleep(3000);
 
 		driver.get("https://www.swiggy.com");
 
 		WebElement locat = driver.findElement(By.id("location"));
 
 		locat.sendKeys("chennai");
-		
-		System.out.println(locat.getAttribute("value"));
+	
+	
+	     System.setProperty("webdriver.chrome.driver","C:\\Users\\NAGA\\OneDrive\\Desktop\\MY CODE\\Selenium-day-2\\driver\\chromedriver.exe");
+			
+			WebDriver driver = new ChromeDriver();
+			
+			driver.manage().window().maximize();
+			
+			driver.get("https://www.swiggy.com");
+			
+			
+		 WebElement locat = driver.findElement(By.id("location"));
+		 
+				
+			locat.sendKeys("chennai");
+			
+			System.out.println(locat.getAttribute("value"));
+			
+			
 
 	}
 }
